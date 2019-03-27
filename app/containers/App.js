a4a9f26e97f3d50c34802ce09899177c63a229a3
Unknown <s1996ch@gmail.com>
2019-03-27 17:55:51 +0430

    Add App container

--- app/containers/App.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import * as React from 'react';
-
-type Props = {
-  children : React.Node
-};
-
-export default (props : Props) => props.children;
---
+++ app/containers/App/index.js
@@ -0,0 +1,59 @@
+// @flow
+
+import React, { Component } from 'react';
+import { autobind } from 'core-decorators';
+import moment from 'moment';
+import { connect } from 'react-redux';
+import { getPhoto } from 'app/containers/Home/redux';
+
+type Props = {
+  children : React.Node,
+  updateWallpaperSchedule : string,
+  updateWallpaperDate : string,
+  getPhotoAction : (data : { data : boolean }) => void,
+};
+
+@connect(
+  state => ({
+    updateWallpaperSchedule: state.getIn(['Settings', 'updateWallpaperSchedule']),
+    updateWallpaperDate: state.getIn(['Settings', 'updateWallpaperDate']),
+  }),
+  {
+    getPhotoAction: getPhoto,
+  },
+)
+@autobind
+class App extends Component<Props> {
+  componentDidMount() : void {
+    const checkUpdateTime = () => {
+      const {
+        updateWallpaperSchedule,
+        updateWallpaperDate,
+        getPhotoAction,
+      } = this.props;
+      switch (updateWallpaperSchedule) {
+        case 'Daily':
+          if ((moment.duration(updateWallpaperDate)).asHours() >= 24) {
+            getPhotoAction({ setAutomaticWallpaper: true });
+          }
+          break;
+        case 'Weekly':
+          if ((moment.duration(updateWallpaperDate)).asHours() >= 168) {
+            getPhotoAction({ setAutomaticWallpaper: true });
+          }
+          break;
+        default:
+          break;
+      }
+    };
+    checkUpdateTime();
+    setInterval(checkUpdateTime, 10000);
+  }
+
+  render() {
+    const { children } = this.props;
+    return children;
+  }
+}
+
+export default App;
