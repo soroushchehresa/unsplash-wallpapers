7905ac99c34f581ccba1c4939eb1d8e04118a43e
Unknown <s1996ch@gmail.com>
2019-03-27 17:54:40 +0430

    Update Home page route name

--- app/Routes.js
+++ app/Routes.js
@@ -3,14 +3,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
-import Main from './containers/Main';
+import Home from './containers/Home';
 import History from './containers/History';
 import Settings from './containers/Settings';
 
 export default () => (
   <App>
     <Switch>
-      <Route path="/" component={Main} exact />
+      <Route path="/" component={Home} exact />
       <Route path="/history" component={History} />
       <Route path="/settings" component={Settings} />
     </Switch>
